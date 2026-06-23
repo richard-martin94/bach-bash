8e1bac86dbb95dc46c2ed96479dc076ff3212761
richard <richard_martin94@mymail.eku.edu>
2026-06-23 14:17:46 -0500

    ef migration attempts

---
+++ Migrations/20260623191602_InitialCreate.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace bach_bash.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "app");
+
+            migrationBuilder.CreateTable(
+                name: "Bashes",
+                schema: "app",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    OwnerId = table.Column<int>(type: "integer", nullable: false),
+                    Created = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    LastModified = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bashes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Challenges",
+                schema: "app",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    Description = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    Points = table.Column<int>(type: "integer", nullable: false),
+                    BashId = table.Column<int>(type: "integer", nullable: false),
+                    Created = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    LastModified = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Challenges", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bashes_Id",
+                schema: "app",
+                table: "Bashes",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Challenges_BashId",
+                schema: "app",
+                table: "Challenges",
+                column: "BashId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bashes",
+                schema: "app");
+
+            migrationBuilder.DropTable(
+                name: "Challenges",
+                schema: "app");
+        }
+    }
+}
